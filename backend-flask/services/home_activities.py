8f0affa9988efbd730faba9e52814dc8558fc641
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-03-14 14:41:20 +0000

    add honeycomb logs for backend STDOUT

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -19,17 +19,17 @@ class HomeActivities:
         'replies_count': 1,
         'reposts_count': 0,
         'replies': [{
-        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
-        'likes_count': 0,
-        'replies_count': 0,
-        'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    },
-    {
+          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+          'handle':  'Worf',
+          'message': 'This post has no honor!',
+          'likes_count': 0,
+          'replies_count': 0,
+          'reposts_count': 0,
+          'created_at': (now - timedelta(days=2)).isoformat()
+        }],
+       },
+       {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
       'handle':  'Worf',
       'message': 'I am out of prune juice',
