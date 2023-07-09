ed92f28f5ab975eed18aba4db1c050857405bd24
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-07-09 18:15:57 +0000

    minor fixes to signinpage.js

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -15,18 +15,17 @@ export default function SigninPage() {
   const onsubmit = async (event) => {
     setErrors('')
     event.preventDefault();
-    try {
-      Auth.signIn(email, password)
-        .then(user => {
-          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
-          window.location.href = "/"
-        })
-    } catch (error) {
-      if (error.code == 'UserNotConfirmedException') {
+    Auth.signIn(email, password)
+    .then(user => {
+     localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+     window.location.href = "/"
+    })
+    .catch(error => {
+        if (error.code == 'UserNotConfirmedException') {
         window.location.href = "/confirm"
       }
       setErrors(error.message)
-    }
+    });
     return false
   }
 
