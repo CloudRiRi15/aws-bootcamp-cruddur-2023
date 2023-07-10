0ca16c77f01aa6409a1f9f8100d35df73a783fca
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-07-10 13:29:09 +0000

    added cors code, headers to app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -81,8 +82,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -154,6 +155,10 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  print(
+    request.headers.get('Authorization') 
+  )
   data = HomeActivities.run()
   return data, 200
 
