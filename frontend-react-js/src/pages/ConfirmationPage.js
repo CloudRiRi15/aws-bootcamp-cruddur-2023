50e50c0935aa222380517e6431c256ecd08f31f5
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-07-21 08:59:50 +0000

    wip

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -32,9 +32,9 @@ const resend_code = async (event) => {
     // does cognito always return english
     // for this to be an okay match?
     console.log(err)
-    if (err.message == 'Username cannot be empty'){
+    if (err.message === 'Username cannot be empty'){
       setErrors("You need to provide an email in order to send Resend Activiation Code")   
-    } else if (err.message == "Username/client id combination not found."){
+    } else if (err.message === "Username/client id combination not found."){
       setErrors("Email is invalid or cannot be found.")   
     }
   }
