c9ce9249ab81fb2fa32656be04b9c90a8a565065
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-07-23 10:50:46 +0000

    query still not working wip

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 #tracer = trace.get_tracer("home.activities")
 
@@ -13,18 +13,31 @@ class HomeActivities:
     #  now = datetime.now(timezone.utc).astimezone()
     #  span.set_attribute("app.now", now.isoformat())
 
-      sql = """"
-      SELECT * FROM activities
-      """    
+    sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
-      print("==-------")    
-      print("==-------") 
-      print(json)    
+          json = cur.fetchone()
+      print("----------")
+      print(json)
       return json[0]      
       return results
    
